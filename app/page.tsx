6964b785120a08998419ae6722f468b9d9f6b3cc
Ivan Espinoza <edgarivanespinoza@gmail.com>
2025-07-03 23:13:42 +0200

    chore: add React Query and Axios for data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main>
+    <>
       <h1>Welcome to the Chess Grandmasters Wiki</h1>
       <p>Your one-stop destination for all things chess grandmasters.</p>
-    </main>
+    </>
   );
 }
